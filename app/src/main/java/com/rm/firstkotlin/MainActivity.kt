5e3a20a6c88c1a936ed6e40f254a7d3ad614f5bc
alulcardino <52010205+alulcardino@users.noreply.github.com>
2022-12-08 15:54:43 +0600

    App is completed

--- app/src/main/java/com/rm/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/rm/firstkotlin/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.rm.firstkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rm.firstkotlin.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var rootElement: ActivityMainBinding
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        rootElement = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(rootElement.root)
+
+        rootElement.editTextName
+
+        rootElement.idButtomSave.setOnClickListener {
+            rootElement.textViewName.text =
+                "Hello, ${rootElement.editTextName.text.toString().trim()} "
+        }
+
+
     }
 }
